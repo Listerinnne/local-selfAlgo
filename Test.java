f2bb6cbf15f0dd1d25fced1ae64812c9dd5f4a0e
Huiju Kwon <00huiju00@gmail.com>
2024-09-03 17:53:34 +0900

    0903

--- Test.java
+++ Test.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Test {
@@ -9,6 +12,10 @@ public class Test {
 		String order = "ddddd";
 		String[] orderArr = order.split(" ");	//공백문자를 기준으로 나눠 배열에 저장
 		System.out.println(orderArr);
+		List<Integer>[] list = new ArrayList[4];
+		Arrays.fill(list, new ArrayList<>());
+		
+		
 
 	}
 
---
+++ boj_9252_huiju.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class boj_9252_huiju {
+	static String strA, strB, tmp1, tmp2;
+	
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		strA = sc.next();
+		strB = sc.next();
+		
+		
+		
+	}
+	//idx1 : 기준이 되는 문자열의 인덱스
+	//idx2 : 비교되는 문자열의 인덱스
+	static void lcs(String str1, String str2, int idx1, int idx2) {
+		int strLen = str1.length();
+		for(int i=0; i<str1.length(); i++) {	//범위 정한 후 재귀
+			
+			
+		}
+		
+	}
+
+}
